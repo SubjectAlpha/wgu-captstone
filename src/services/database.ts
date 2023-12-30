021d46ab79fb674c5edcd166cecdd2ac7581bbec
Jake Starr <jakestarr60@gmail.com>
2023-12-30 00:37:16 -0500

    add prisma pt2

--- src/services/database.ts
+++
@@ -1,9 +0,0 @@
-import { env } from "process";
-import mysql from "mysql2";
-
-export default mysql.createConnection({
-    host: env.MYSQL_SERVER,
-    user: env.MYSQL_USER,
-    password: env.MYSQL_PASSWORD,
-    database: env.MYSQL_DATABASE
-});
